aed64fc58f0f4960b992f6a94a123cfcdc59af2d
YoniWoo <44089815+YoniWoo@users.noreply.github.com>
2018-12-14 02:58:01 +0100

    Delete video.php

--- video.php
+++
@@ -1,7 +0,0 @@
-<?php
-include ('init.inc.php');
-$mysqliResult = $mysqli->query("SELECT * FROM video WHERE id_video ='1'");
-$result = $mysqliResult->fetch_assoc();
-
-$contenu .= '<video src="'.$result['video'].'"></video>'
-?>
